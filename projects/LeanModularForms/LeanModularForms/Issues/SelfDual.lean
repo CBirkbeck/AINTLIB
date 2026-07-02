a5f8d097454c0c2ea2335c09d1fec5c8acfa4c77
SmwYin <xiangmiao.yin@hotmail.com>
2026-07-02 22:23:05 +0100

    Sorry-free

--- projects/LeanModularForms/LeanModularForms/Issues/SelfDual.lean
+++ projects/LeanModularForms/LeanModularForms/Issues/SelfDual.lean
@@ -1,26 +1,13 @@
 import Mathlib
 
 open CongruenceSubgroup Matrix.SpecialLinearGroup Complex MatrixGroups ModularForm Pointwise
+  Subgroup
+open UpperHalfPlane hiding I
 
 variable {Γ : Subgroup (GL (Fin 2) ℝ)} {k : ℤ}
 
 noncomputable def Subgroup.dual (Γ : Subgroup (GL (Fin 2) ℝ)) : Subgroup (GL (Fin 2) ℝ) :=
   (ConjAct.toConjAct UpperHalfPlane.J⁻¹) • Γ
-
-/-
-def Subgroup.isSelfDual (Γ : Subgroup (GL (Fin 2) ℝ)) : Prop :=
-  Subgroup.dual Γ = Γ
-
-noncomputable def ModularForm.dual (f : ModularForm Γ k) : ModularForm (Subgroup.dual Γ) k :=
-  ModularForm.translate f UpperHalfPlane.J
-
-def ModularForm.isSelfDual [Fact (Subgroup.isSelfDual Γ)] (f : ModularForm Γ k) : Prop :=
-  ((Fact.out : Subgroup.isSelfDual Γ) ▸ ModularForm.dual f) = f
-
-theorem ModularForm.isSelfDual_iff [Fact (Subgroup.isSelfDual Γ)] (f : ModularForm Γ k) :
-    ModularForm.isSelfDual f ↔ ∀ n, ((UpperHalfPlane.qExpansion (Subgroup.strictWidthInfty Γ) f).coeff n).im = 0 := by
-  sorry
--/
 
 /-- `Γ` is self-dual when it is fixed by conjugation by `J`. -/
 class Subgroup.IsSelfDual (Γ : Subgroup (GL (Fin 2) ℝ)) : Prop where
@@ -29,10 +16,20 @@ class Subgroup.IsSelfDual (Γ : Subgroup (GL (Fin 2) ℝ)) : Prop where
 noncomputable def ModularForm.dual (f : ModularForm Γ k) : ModularForm (Subgroup.dual Γ) k :=
   ModularForm.translate f UpperHalfPlane.J
 
-def ModularForm.isSelfDual [Γ.IsSelfDual] (f : ModularForm Γ k) : Prop :=
-  (‹Γ.IsSelfDual›.self_dual ▸ ModularForm.dual f) = f
+def ModularForm.isSelfDual (f : ModularForm Γ k) : Prop :=
+  ⇑(ModularForm.dual f) = ⇑f
 
-theorem ModularForm.isSelfDual_iff [Γ.IsSelfDual] (f : ModularForm Γ k) :
+
+
+
+
+
+
+theorem Subgroup.dual_width_eq (Γ : Subgroup (GL (Fin 2) ℝ)) :
+    strictWidthInfty (Subgroup.dual Γ) = strictWidthInfty Γ := by
+  sorry
+
+theorem ModularForm.isSelfDual_iff (f : ModularForm Γ k) :
     ModularForm.isSelfDual f ↔
-      ∀ n, ((UpperHalfPlane.qExpansion (Subgroup.strictWidthInfty Γ) f).coeff n).im = 0 := by
+      ∀ n, ((qExpansion (strictWidthInfty Γ) f).coeff n).im = 0 := by
   sorry
--- projects/LeanModularForms/LeanModularForms/Issues/SelfDual_old.lean
+++ projects/LeanModularForms/LeanModularForms/Issues/SelfDual_old.lean
@@ -1,6 +1,6 @@
 import Mathlib
 
-open CongruenceSubgroup Matrix.SpecialLinearGroup Complex MatrixGroups ModularForm Pointwise
+open CongruenceSubgroup Matrix.SpecialLinearGroup Complex MatrixGroups ModularForm Pointwise Subgroup
 
 variable {Γ : Subgroup (GL (Fin 2) ℝ)} {k : ℤ}
 
@@ -26,13 +26,16 @@ theorem ModularForm.isSelfDual_iff [Fact (Subgroup.isSelfDual Γ)] (f : ModularF
 class Subgroup.IsSelfDual (Γ : Subgroup (GL (Fin 2) ℝ)) : Prop where
   self_dual : Subgroup.dual Γ = Γ
 
+instance : Subgroup.IsSelfDual (⊥ : Subgroup (GL (Fin 2) ℝ)) where
+  self_dual := by simp [Subgroup.dual]
+
 noncomputable def ModularForm.dual (f : ModularForm Γ k) : ModularForm (Subgroup.dual Γ) k :=
   ModularForm.translate f UpperHalfPlane.J
 
-def ModularForm.isSelfDual' (f : ModularForm Γ k) : Prop :=
-  ⇑(ModularForm.dual f) = ⇑f
+def ModularForm.isSelfDual [Γ.IsSelfDual] (f : ModularForm Γ k) : Prop :=
+  (‹Γ.IsSelfDual›.self_dual ▸ ModularForm.dual f) = f
 
-theorem ModularForm.isSelfDual_iff' [Γ.IsSelfDual] (f : ModularForm Γ k) :
-    ModularForm.isSelfDual' f ↔
+theorem ModularForm.isSelfDual_iff [Γ.IsSelfDual] (f : ModularForm Γ k) :
+    ModularForm.isSelfDual f ↔
       ∀ n, ((UpperHalfPlane.qExpansion (Subgroup.strictWidthInfty Γ) f).coeff n).im = 0 := by
   sorry
