135554c78961719091acc8ebe9fc8e72d3a3586d
Chris Birkbeck <cd.birkbeck@gmail.com>
2026-07-11 23:52:46 +0100

    feat(Y1-CLOSER): BB-FLAT N5/G2 — kernel_div_of_chartFactor (PIECE-DIV): divisibility over chart-factoring tests via the K3 transport chain (baseChangeEquiv.symm ≪ pointAddEquiv, restrict-compat by val-chases through chartZero_totalIso/chartZero_fst) (NEW-Y1)

--- projects/ModularCurves/ModularCurves/EllipticCurve/KernelDivisibilityGlue.lean
+++ projects/ModularCurves/ModularCurves/EllipticCurve/KernelDivisibilityGlue.lean
@@ -101,6 +101,110 @@ theorem modelKernel_inj {R S' : CommRingCat.{u}} {φ : R ⟶ S'}
 
 end ModelInstance
 
+section PieceTransport
+
+variable {S : Scheme.{u}} (E : EllipticCurve S)
+
+/-- **(PIECE-DIV)** Kernel divisibility over a test factoring through an atlas chart:
+transport along the base-change/pointed-iso chain (hμ from K3) to the model record and
+apply MODEL-DIV. -/
+theorem kernel_div_of_chartFactor (A : WeierstrassAtlasData E.toEllipticCurveGeom)
+    (i : A.ι) {R S' : CommRingCat.{u}} {φ : R ⟶ S'}
+    (hφ : Function.Surjective φ.hom) (hφ2 : RingHom.ker φ.hom ^ 2 = ⊥)
+    (tB : Spec R ⟶ Spec Γ(S, (A.U i).1))
+    (N : ℕ) (hN : IsUnit ((N : ℕ) : ↑R))
+    (P : E.Point (tB ≫ chartToBase A i))
+    (hP : Point.restrict E (Spec.map φ) P = 0) :
+    ∃ δ : E.Point (tB ≫ chartToBase A i),
+      Point.restrict E (Spec.map φ) δ = 0 ∧ (N : ℤ) • δ = P := by
+  classical
+  haveI := A.elliptic i
+  -- the transport chain
+  have hη : (η[(E.baseChange (chartToBase A i)).asOver] :
+      𝟙_ (Over (Spec Γ(S, (A.U i).1))) ⟶ (E.baseChange (chartToBase A i)).asOver) ≫
+      (chartOverIso A i).hom = η[(modelEllipticCurve (A.W i)).asOver] :=
+    chartGrp_one A E.grp E.one_eq_zero i
+  have hμ := isMonHom_of_pointedIso_records (E.baseChange (chartToBase A i))
+    (modelEllipticCurve (A.W i)) (chartOverIso A i) hη
+  set eqc : E.Point (tB ≫ chartToBase A i) ≃+
+      (modelEllipticCurve (A.W i)).Point tB :=
+    (Point.baseChangeEquiv E (chartToBase A i) tB).symm.trans
+      (pointAddEquiv (chartOverIso A i) hμ tB) with heqc
+  have heqc_coe : ∀ Q : E.Point (tB ≫ chartToBase A i),
+      (eqc Q).1 = pullback.lift Q.1 tB Q.2 ≫ (chartTotalIso A i).hom := fun Q => rfl
+  have heqc_symm_coe : ∀ Qm : (modelEllipticCurve (A.W i)).Point tB,
+      (eqc.symm Qm).1 = (Qm.1 ≫ (chartTotalIso A i).inv) ≫
+        pullback.fst E.π (chartToBase A i) := fun Qm => rfl
+  -- transported kernel membership
+  have hPm : Point.restrict (modelEllipticCurve (A.W i)) (Spec.map φ) (eqc P) = 0 := by
+    refine Subtype.ext ?_
+    show Spec.map φ ≫ (eqc P).1 = _
+    rw [(modelEllipticCurve (A.W i)).point_zero_val]
+    rw [heqc_coe]
+    have hPval : Spec.map φ ≫ P.1 =
+        (Spec.map φ ≫ (tB ≫ chartToBase A i)) ≫ E.zero := by
+      have h1 := congrArg Subtype.val hP
+      rw [show ((0 : E.Point (Spec.map φ ≫ (tB ≫ chartToBase A i))) :
+        Spec S' ⟶ E.E) = (Spec.map φ ≫ (tB ≫ chartToBase A i)) ≫ E.zero from
+        E.point_zero_val _] at h1
+      exact h1
+    -- the restricted lift is the chart zero lift
+    have hliftres : Spec.map φ ≫ pullback.lift P.1 tB P.2 =
+        (Spec.map φ ≫ tB) ≫ chartZero A i := by
+      refine pullback.hom_ext ?_ ?_
+      · rw [Category.assoc, pullback.lift_fst, Category.assoc, Category.assoc]
+        rw [show chartZero A i ≫ pullback.fst E.toEllipticCurveGeom.π (chartToBase A i) =
+          chartToBase A i ≫ E.toEllipticCurveGeom.zero from chartZero_fst A i]
+        rw [hPval]
+        simp only [Category.assoc]
+      · rw [Category.assoc, pullback.lift_snd, Category.assoc, Category.assoc]
+        rw [show chartZero A i ≫ pullback.snd E.toEllipticCurveGeom.π (chartToBase A i) =
+          𝟙 _ from chartZero_snd A i]
+        rw [Category.comp_id]
+    refine ((Category.assoc _ _ _).symm).trans ?_
+    refine (congrArg (· ≫ (chartTotalIso A i).hom) hliftres).trans ?_
+    refine (Category.assoc _ _ _).trans ?_
+    refine (congrArg ((Spec.map φ ≫ tB) ≫ ·) (chartZero_totalIso A i)).trans ?_
+    rfl
+  -- solve at the model
+  obtain ⟨δm, hδmK, hδmN⟩ := modelKernel_div (A.W i) hφ hφ2 N hN (eqc P) hPm
+  refine ⟨eqc.symm δm, ?_, ?_⟩
+  · -- kernel membership pulls back
+    refine Subtype.ext ?_
+    show Spec.map φ ≫ (eqc.symm δm).1 = _
+    rw [E.point_zero_val]
+    rw [heqc_symm_coe]
+    have hδmval : Spec.map φ ≫ δm.1 = (Spec.map φ ≫ tB) ≫ projModelZero (A.W i) := by
+      have h1 := congrArg Subtype.val hδmK
+      rw [show ((0 : (modelEllipticCurve (A.W i)).Point (Spec.map φ ≫ tB)) :
+        Spec S' ⟶ (modelEllipticCurve (A.W i)).E) =
+        (Spec.map φ ≫ tB) ≫ projModelZero (A.W i) from
+        (modelEllipticCurve (A.W i)).point_zero_val _] at h1
+      exact h1
+    have hzeroinv : projModelZero (A.W i) ≫ (chartTotalIso A i).inv = chartZero A i := by
+      rw [← chartZero_totalIso A i, Category.assoc, Iso.hom_inv_id, Category.comp_id]
+    refine ((Category.assoc _ _ _).symm).trans ?_
+    refine (congrArg (· ≫ pullback.fst E.π (chartToBase A i))
+      ((Category.assoc _ _ _).symm)).trans ?_
+    refine (congrArg (fun m => (m ≫ (chartTotalIso A i).inv) ≫
+      pullback.fst E.π (chartToBase A i)) hδmval).trans ?_
+    refine (congrArg (· ≫ pullback.fst E.π (chartToBase A i))
+      (Category.assoc _ _ _)).trans ?_
+    refine (congrArg (· ≫ pullback.fst E.π (chartToBase A i))
+      (congrArg ((Spec.map φ ≫ tB) ≫ ·) hzeroinv)).trans ?_
+    refine (Category.assoc _ _ _).trans ?_
+    refine (congrArg ((Spec.map φ ≫ tB) ≫ ·) (chartZero_fst A i)).trans ?_
+    refine ((Category.assoc _ _ _).symm).trans ?_
+    exact Category.assoc _ _ _
+  · -- the scalar identity pulls back
+    have h1 : eqc ((N : ℤ) • eqc.symm δm) = eqc P := by
+      rw [map_zsmul, AddEquiv.apply_symm_apply, hδmN]
+    exact eqc.injective h1
+
+end PieceTransport
+
+
+
 end EllipticCurve
 
 end ModularCurves
