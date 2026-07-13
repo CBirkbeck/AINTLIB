c1e6eaa1ced9d63cd9ddc8c1c369c922d1913bb8
Chris Birkbeck <cd.birkbeck@gmail.com>
2026-07-13 15:08:28 +0100

    feat(ModularCurves): spread affine open immersions

--- projects/ModularCurves/ModularCurves.lean
+++ projects/ModularCurves/ModularCurves.lean
@@ -45,6 +45,7 @@ import ModularCurves.ForMathlib.ComoduleCoinvariants
 import ModularCurves.ForMathlib.FiniteAffineOpenCover
 import ModularCurves.ForMathlib.FinitePresentationAwayMap
 import ModularCurves.ForMathlib.FinitePresentationAwayMapFamily
+import ModularCurves.ForMathlib.FinitePresentationOpenImmersionFamily
 import ModularCurves.ForMathlib.FinitePresentationBaseChangeEquiv
 import ModularCurves.ForMathlib.FinitePresentationEquiv
 import ModularCurves.ForMathlib.FinitePresentationFunctor
