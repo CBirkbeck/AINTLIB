4b568850fff0a8fb5704c017ad9c561f518a603d
Chris Birkbeck <cd.birkbeck@gmail.com>
2026-07-14 00:28:13 +0100

    feat(ModularCurves): restrict sheaves to open subspaces

--- projects/ModularCurves/ModularCurves.lean
+++ projects/ModularCurves/ModularCurves.lean
@@ -50,6 +50,7 @@ import ModularCurves.ForMathlib.OpensMapFinal
 import ModularCurves.ForMathlib.ProjIntegral
 import ModularCurves.ForMathlib.SpecQuotientIso
 import ModularCurves.ForMathlib.SurjectiveFreeSameRank
+import ModularCurves.ForMathlib.TopCatSheafRestrict
 import ModularCurves.ForMathlib.PullbackLocalAtTarget
 import ModularCurves.ForMathlib.PullbackUnitMonoidal
 import ModularCurves.ForMathlib.QuotientCurveModel
