fb69f278848e84e51029e71871a9c81cac383836
Chris Birkbeck <cd.birkbeck@gmail.com>
2026-07-14 12:26:31 +0100

    feat(ModularCurves): reflect away-map bijectivity

--- projects/ModularCurves/ModularCurves.lean
+++ projects/ModularCurves/ModularCurves.lean
@@ -46,6 +46,7 @@ import ModularCurves.ForMathlib.AffineVanishing
 import ModularCurves.ForMathlib.ClosedImmersionFinrankIso
 import ModularCurves.ForMathlib.ComoduleCoinvariants
 import ModularCurves.ForMathlib.FiniteAffineOpenCover
+import ModularCurves.ForMathlib.FinitePresentationAwayMap
 import ModularCurves.ForMathlib.FinitePresentationBaseChangeEquiv
 import ModularCurves.ForMathlib.FinitePresentationEquiv
 import ModularCurves.ForMathlib.FinitePresentationFunctor
