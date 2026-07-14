9297bbc9557f911c6f691d7c85a15295b1a41090
Chris Birkbeck <cd.birkbeck@gmail.com>
2026-07-14 01:38:09 +0100

    feat(ModularCurves): package acyclic affine covers

--- projects/ModularCurves/ModularCurves.lean
+++ projects/ModularCurves/ModularCurves.lean
@@ -40,6 +40,7 @@ import ModularCurves.ForMathlib.BaseChangeKerCoker
 import ModularCurves.ForMathlib.Coaction
 import ModularCurves.ForMathlib.SchemeAppLE
 import ModularCurves.ForMathlib.AgreementLocusClopen
+import ModularCurves.ForMathlib.AcyclicAffineOpenCover
 import ModularCurves.ForMathlib.AffineVanishing
 import ModularCurves.ForMathlib.ClosedImmersionFinrankIso
 import ModularCurves.ForMathlib.ComoduleCoinvariants
