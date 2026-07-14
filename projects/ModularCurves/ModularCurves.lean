a5edff3aaedb2df3cdd42e8be89b4e9d5c98ac5f
Chris Birkbeck <cd.birkbeck@gmail.com>
2026-07-14 12:09:23 +0100

    feat(ModularCurves): spread principal affine opens

--- projects/ModularCurves/ModularCurves.lean
+++ projects/ModularCurves/ModularCurves.lean
@@ -46,6 +46,7 @@ import ModularCurves.ForMathlib.ClosedImmersionFinrankIso
 import ModularCurves.ForMathlib.ComoduleCoinvariants
 import ModularCurves.ForMathlib.FiniteAffineOpenCover
 import ModularCurves.ForMathlib.FinitePresentationFunctor
+import ModularCurves.ForMathlib.FinitePresentationLocalization
 import ModularCurves.ForMathlib.FiniteLocallyFreeIsoLocus
 import ModularCurves.ForMathlib.FlasqueCohomology
 import ModularCurves.ForMathlib.GaloisDescentModule
