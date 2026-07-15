333b922fd4f47f5e65bb4ba514888b7395016c37
Chris Birkbeck <c.birkbeck@uea.ac.uk>
2026-07-15 05:57:26 +0100

    cleanup(HasseWeil): unfold→simp only (3) in Curves/Divisor/MillerAllChar (#6450) (#6537)

--- projects/HasseWeil/HasseWeil/Foundation/Curves/Differentials.lean
+++ projects/HasseWeil/HasseWeil/Foundation/Curves/Differentials.lean
@@ -3,9 +3,9 @@ Copyright (c) 2026 Chris Birkbeck. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Birkbeck
 -/
-import HasseWeil.Foundation.Auxiliary.PullbackKaehler
 import HasseWeil.Foundation.Curves.Basic
 import HasseWeil.Foundation.Curves.Transcendence
+import HasseWeil.Foundation.Auxiliary.PullbackKaehler
 import HasseWeil.Foundation.InvariantDifferentialPullback
 import HasseWeil.Foundation.OmegaPullbackCoeff
 import HasseWeil.Isogeny.Kernel
