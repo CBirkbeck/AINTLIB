5ad4766dff1cda4c763fec7d375ae0d733b2da65
Chris Birkbeck <cd.birkbeck@gmail.com>
2026-07-20 21:42:00 +0900

    Identify projective twist Cech factors

---
+++ projects/ModularCurves/ModularCurves/EllipticCurve/ProjectiveSpaceTwistCech.lean
@@ -0,0 +1,83 @@
+/-
+Copyright (c) 2026 The AINTLIB contributors. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: AINTLIB ModularCurves project
+-/
+import ModularCurves.EllipticCurve.ProjectiveSpaceTwist
+import ModularCurves.ForMathlib.SchemeModuleOrderedBaseCech
+
+/-!
+# Cech factors for twists on polynomial projective space
+
+This file begins the ordered standard-cover calculation of the cohomology of
+projective-space twists. It identifies the sections of `O(d)` on each ordered
+intersection with the sections of the structure sheaf there.
+-/
+
+namespace MvPolynomial
+
+open AlgebraicGeometry CategoryTheory CategoryTheory.Limits Opposite
+
+noncomputable section
+
+universe u
+
+variable {R : Type u} {σ : Type} [CommRing R]
+
+attribute [local instance] MvPolynomial.gradedAlgebra
+
+/-- The standard coordinate cover, lifted to the universe of the projective scheme. -/
+abbrev coordinateOpenCover (i : ULift.{u} σ) :
+    (Proj (homogeneousSubmodule σ R)).Opens :=
+  coordinateOpen (R := R) i.down
+
+/-- The intersection of the standard coordinate charts indexed by a Cech tuple. -/
+abbrev coordinateOpenCechIntersection {n : ℕ}
+    (a : Fin (n + 1) → ULift.{u} σ) :
+    (Proj (homogeneousSubmodule σ R)).Opens :=
+  ∏ᶜ fun k : Fin (n + 1) => coordinateOpenCover (R := R) (a k)
+
+/-- A standard Cech intersection is contained in each chart occurring in its tuple. -/
+theorem coordinateOpenCechIntersection_le {n : ℕ}
+    (a : Fin (n + 1) → ULift.{u} σ)
+    (k : Fin (n + 1)) :
+    coordinateOpenCechIntersection (R := R) a ≤ coordinateOpenCover (R := R) (a k) :=
+  leOfHom (Pi.π (fun l : Fin (n + 1) => coordinateOpenCover (R := R) (a l)) k)
+
+/-- The standard frame of `O(d)` restricted to an ordered Cech intersection. -/
+noncomputable def coordinateHyperplaneTwistCechTrivialization {n : ℕ}
+    (a : Fin (n + 1) → ULift.{u} σ) (j : σ) (d : ℤ) :
+    (coordinateHyperplaneTwist (R := R) j d).restrict
+        (coordinateOpenCechIntersection (R := R) a).ι ≅
+      Scheme.Modules.unitObj
+        (coordinateOpenCechIntersection (R := R) a).toScheme :=
+  Scheme.Modules.restrictIsoOfPullbackIso
+    (coordinateHyperplaneTwist (R := R) j d)
+    (coordinateOpenCechIntersection (R := R) a)
+    (Scheme.Modules.restrictTrivialization
+      (coordinateOpenCechIntersection_le (R := R) a 0)
+      (Scheme.Modules.pullbackIsoOfRestrictIso
+        (coordinateHyperplaneTwist (R := R) j d)
+        (coordinateOpenCover (R := R) (a 0))
+        (coordinateHyperplaneTwistTrivialization (R := R) (a 0).down j d)))
+
+/-- A twist Cech factor is the corresponding structure-sheaf section module. -/
+noncomputable def coordinateHyperplaneTwistBaseCechFactorIsoUnit {n : ℕ}
+    (a : Fin (n + 1) → ULift.{u} σ) (j : σ) (d : ℤ) :
+    Scheme.Modules.baseCechFactor
+        (homogeneousProjπ (R := R) (σ := σ))
+        (coordinateHyperplaneTwist (R := R) j d)
+        (coordinateOpenCover (R := R) (σ := σ)) n a ≅
+      Scheme.Modules.baseCechFactor
+        (homogeneousProjπ (R := R) (σ := σ))
+        (Scheme.Modules.unitObj (Proj (homogeneousSubmodule σ R)))
+        (coordinateOpenCover (R := R) (σ := σ)) n a :=
+  Scheme.Modules.baseModulePresheafObjIsoUnitOfRestrictIso
+    (homogeneousProjπ (R := R) (σ := σ))
+    (coordinateHyperplaneTwist (R := R) j d)
+    (coordinateOpenCechIntersection (R := R) a)
+    (coordinateHyperplaneTwistCechTrivialization (R := R) a j d)
+
+end
+
+end MvPolynomial
