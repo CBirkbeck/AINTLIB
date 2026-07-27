db5a67ae5130aae1d9a2b62372c5e1a52c0483cd
Chris Birkbeck <cd.birkbeck@gmail.com>
2026-07-27 07:59:55 +0100

    Compute sections of canonical affine charts

--- projects/ModularCurves/ModularCurves/ForMathlib/SchemeAppLE.lean
+++ projects/ModularCurves/ModularCurves/ForMathlib/SchemeAppLE.lean
@@ -3,6 +3,7 @@ Copyright (c) 2026 Chris Birkbeck. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Chris Birkbeck
 -/
+import Mathlib.AlgebraicGeometry.AffineScheme
 import Mathlib.AlgebraicGeometry.Restrict
 
 /-!
@@ -53,5 +54,33 @@ theorem appLE_congr_hom {X Y : Scheme.{u}} {f g : X ⟶ Y} (h : f = g) (U : Y.Op
     (W : X.Opens) (e : W ≤ f ⁻¹ᵁ U) :
     f.appLE U W e = g.appLE U W (h ▸ e) := by
   subst h; rfl
+
+/-- The canonical affine-chart morphism induces the inverse `ΓSpec` identification on
+sections. -/
+theorem IsAffineOpen.fromSpec_appLE_comp_ΓSpecIso
+    {X : Scheme.{u}} {U : X.Opens} (hU : IsAffineOpen U) :
+    hU.fromSpec.appLE U ⊤
+        (le_of_eq hU.fromSpec_preimage_self.symm) ≫
+      (Scheme.ΓSpecIso Γ(X, U)).hom =
+        𝟙 Γ(X, U) := by
+  have hdef : hU.fromSpec = hU.isoSpec.inv ≫ U.ι := rfl
+  rw [appLE_congr_hom hdef U ⊤,
+    ← Scheme.Hom.appLE_comp_appLE hU.isoSpec.inv U.ι U ⊤ ⊤
+      U.ι_preimage_self.ge le_top,
+    ι_appLE_top,
+    appLE_top_top]
+  have h1 : hU.isoSpec.inv.appTop ≫ hU.isoSpec.hom.appTop = 𝟙 _ := by
+    rw [← Scheme.Hom.comp_appTop, Iso.hom_inv_id, Scheme.Hom.id_appTop]
+  have h2 : hU.isoSpec.hom.appTop =
+      (Scheme.ΓSpecIso Γ(X, U)).hom ≫ U.topIso.inv := by
+    rw [hU.isoSpec_hom, Scheme.Opens.toSpecΓ_appTop]
+  rw [h2] at h1
+  have h3 : hU.isoSpec.inv.appTop ≫
+      (Scheme.ΓSpecIso Γ(X, U)).hom = U.topIso.hom := by
+    have h4 := congrArg (· ≫ U.topIso.hom) h1
+    simp only [Category.assoc, Iso.inv_hom_id, Category.comp_id,
+      Category.id_comp] at h4
+    exact h4
+  rw [Category.assoc, h3, Iso.inv_hom_id]
 
 end AlgebraicGeometry
